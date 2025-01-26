1db9d6aef204750f6d15c4b69eab16be741c920e
Kimrama <kimrama0x@gmail.com>
2025-01-26 19:52:11 +0700

    create auth layout

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,4 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using EventListener.Models;
 
 namespace EventListener.Controllers;
 
--- obj/Debug/net9.0/EventListener.AssemblyInfo.cs
+++ obj/Debug/net9.0/EventListener.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventListener")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f867afecb9841480b8eb34b4cf0b42f049470e8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04b2027aa024771a13e7c9e55df40ba9fb22d402")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventListener")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventListener")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

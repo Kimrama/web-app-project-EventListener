3a7663129a4d2acd3a6ee4174657b0bfbbc4fe4f
jackpencwby <kajonyod.jack@gmail.com>
2025-02-08 19:27:05 +0700

    connect-front-and-back-activity-details-remain-userjoin

---
+++ ViewModels/Activity/ActivityDetailViewModel.cs
@@ -0,0 +1,8 @@
+using EventListener.Models;
+
+public class ActivityDetailViewModel
+{
+    public Activity Activity { get; set; }
+    public UserJoinActivity UserJoinActivity { get; set; }
+    public int UserJoinActivityCount { get; set; }
+}

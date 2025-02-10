88e6fef6e45a1734a6bf104755666c63eb9b0197
tawan123456789 <tawan.psm63@gmail.com>
2025-02-10 12:37:08 +0700

    merge main

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using dotnetcoreidentity.Data;
 using EventListener.Models;
 using System.Linq;
-using System.Security.Claims;
 using System.Threading.Tasks;
 
 public class ProfileController : Controller

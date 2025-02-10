5162d87868f275686af03e97f384c59dd042b8f0
tawan123456789 <tawan.psm63@gmail.com>
2025-02-11 01:40:08 +0700

    fix bug Model is not valid

--- Models/User.cs
+++ Models/User.cs
@@ -5,17 +5,17 @@ namespace EventListener.Models;
 public class User : IdentityUser
 {
     //pk = UserName
-    public string Firstname  { get; set; }
-    public string Lastname { get; set; }
-    public string Nickname { get; set; }
-    public DateOnly Birthday { get; set; }
+    public string? Firstname  { get; set; }
+    public string? Lastname { get; set; }
+    public string? Nickname { get; set; }
+    public DateOnly? Birthday { get; set; }
     public string Sex { get; set; }
     public string? About { get; set; }
     public string? UserImageUrl { get; set; }
 
-    public ICollection<Activity> Activities { get; set; }
-    public ICollection<UserInterestActivityTag> UserInterestActivityTags { get; set; }
-    public ICollection<UserJoinActivity> UserJoinActivities { get; set; }
-    public ICollection<Notification> Notifications { get; set; }
-    public ICollection<ChatMessage> ChatMessages { get; set; }
+    public ICollection<Activity>? Activities { get; set; }
+    public ICollection<UserInterestActivityTag>? UserInterestActivityTags { get; set; }
+    public ICollection<UserJoinActivity>? UserJoinActivities { get; set; }
+    public ICollection<Notification>? Notifications { get; set; }
+    public ICollection<ChatMessage>? ChatMessages { get; set; }
 }

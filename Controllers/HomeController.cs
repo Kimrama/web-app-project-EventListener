054998a20e5e44855f1b44f988cbce13e9fa30f8
Kimrama <kimrama0x@gmail.com>
2025-02-13 16:47:40 +0700

    complete activity data to create card

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,11 +11,17 @@ public class HomeController : Controller
     private readonly UserManager<User> _userManager;
     private readonly ApplicationDbContext _context;
 
+
     public HomeController(ILogger<HomeController> logger, UserManager<User> userManager, ApplicationDbContext context)
     {
         _logger = logger;
         _userManager = userManager;
         _context = context;
+    }
+    public string EncodeBase64(string input)
+    {
+        var bytes = System.Text.Encoding.UTF8.GetBytes(input);
+        return Convert.ToBase64String(bytes);
     }
     [HttpGet]
     public async Task<IActionResult> Index()
@@ -24,6 +30,21 @@ public class HomeController : Controller
         .Include(a => a.ActivityTag)
         .Include(b => b.UserJoinActivities)
         .ToListAsync();
-        return View(activities);
+        
+        var activityViewModels = activities.Select(c => new ActivityViewModel
+        {
+            ActivityIdEncode = EncodeBase64(c.OwnerId + " " + c.CreatedAt.ToString("yyyy-MM-dd HH:mm:ss.fffffff")),
+            ActivityTagId = c.ActivityTagId,
+            ActivityName = c.ActivityName,
+            Location = c.Location,
+            StartDate = c.StartDate,
+            StartTime = c.StartTime,
+            ParticipantLimit = c.ParticipantLimit,
+            ActivityImageUrl = c.ActivityImageUrl,
+            ActivityTagCategory = c.ActivityTag.Category,
+            UserJoinActivityCount = c.UserJoinActivities.Count()
+        }).ToList();
+
+        return View(activityViewModels);
     }
 }

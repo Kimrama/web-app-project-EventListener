ab38fa2666d0485e5d44c2065841bd38984f6965
Kimrama <kimrama0x@gmail.com>
2025-02-13 14:49:29 +0700

    fetch activity data from database

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 using EventListener.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
-namespace EventListener.Controllers;
+using EventListener.Data;
+using Microsoft.EntityFrameworkCore;
 
+namespace EventListener.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
     private readonly UserManager<User> _userManager;
+    private readonly ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger, UserManager<User> userManager)
+    public HomeController(ILogger<HomeController> logger, UserManager<User> userManager, ApplicationDbContext context)
     {
         _logger = logger;
         _userManager = userManager;
+        _context = context;
     }
-
-    public IActionResult Index()
+    [HttpGet]
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var activities = await _context.Activities.ToListAsync();
+        return View(activities);
     }
 }

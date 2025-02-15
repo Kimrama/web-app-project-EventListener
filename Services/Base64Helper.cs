59ce0317d01fcb70ea01f1eb260e89c43d74015b
Kimrama <kimrama0x@gmail.com>
2025-02-15 17:42:02 +0700

    create Encode-Decode base 64 service

---
+++ Services/Base64Helper.cs
@@ -0,0 +1,27 @@
+using System.Text;
+
+namespace EventListener.Services;
+
+public class Base64Helper
+{
+    public string EncodeBase64(string input)
+    {
+        var bytes = Encoding.UTF8.GetBytes(input);
+        return Convert.ToBase64String(bytes)
+            .Replace("+", "-")  
+            .Replace("/", "_") 
+            .TrimEnd('=');
+    }
+
+    public string DecodeBase64(string input)
+    {
+        string base64 = input.Replace("-", "+").Replace("_", "/"); 
+        while (base64.Length % 4 != 0)
+        {
+            base64 += "=";
+        }
+        
+        var bytes = Convert.FromBase64String(base64);
+        return Encoding.UTF8.GetString(bytes);
+    }  
+}

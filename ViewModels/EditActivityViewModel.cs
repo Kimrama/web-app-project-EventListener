1e67b2f28fb5340cb72565dfefa661d4a002b3ea
jackpencwby <kajonyod.jack@gmail.com>
2025-02-26 22:47:00 +0700

    edit-activity-success-and-permission-for-edit

---
+++ ViewModels/EditActivityViewModel.cs
@@ -0,0 +1,10 @@
+public class EditActivityViewModel
+{
+    public string ActivityId { get; set; }
+    public string ActivityName { get; set; }
+    public string ActivityTag { get; set; }
+    public string Location { get; set; }
+    public DateTime StartDateTime { get; set; }
+    public int ParticipantLimit { get; set; }
+    public string Detail { get; set; }
+}

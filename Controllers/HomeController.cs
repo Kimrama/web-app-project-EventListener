86c3a60b490a3ea677be083612b20a95ed79e805
Kimrama <kimrama0x@gmail.com>
2025-03-01 16:15:42 +0700

    create FilterActivityData endpoint for filter

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -98,4 +98,62 @@ public async Task<JsonResult> FetchActivityData()
     return Json(activities);
 }
 
+[HttpGet]
+public async Task<JsonResult> FilterActivityData(DateTime? startDate, DateTime? endDate, string[]? tags, string? searchWord)
+{
+
+    var query = _context.Activities
+        .Include(a => a.ActivityTag)
+        .Include(a => a.UserJoinActivities)
+            .ThenInclude(uja => uja.User)
+        .AsQueryable();
+
+    if (startDate.HasValue)
+    {
+        var startDateOnly = DateOnly.FromDateTime(startDate.Value);
+        query = query.Where(a => a.StartDate >= startDateOnly);
+    }
+    if (endDate.HasValue)
+    {
+        var endDateOnly = DateOnly.FromDateTime(endDate.Value);
+        query = query.Where(a => a.StartDate <= endDateOnly);
+    }
+
+    if (tags != null && tags.Any())
+    {
+        query = query.Where(a => tags.Contains(a.ActivityTagId));  // กรองตาม ActivityTagId
+    }
+
+    if (!string.IsNullOrEmpty(searchWord))
+    {
+        query = query.Where(a => a.ActivityName.Contains(searchWord));
+    }
+
+    var activities = await query.OrderByDescending(a => a.CreatedAt)
+        .Select(c => new ActivityViewModel
+        {
+            ActivityIdEncode = Base64Helper.EncodeBase64(c.OwnerId + " " + c.CreatedAt.ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture)),
+            ActivityTagId = c.ActivityTagId,
+            ActivityName = c.ActivityName,
+            Location = c.Location,
+            StartDate = c.StartDate,
+            StartTime = c.StartTime,
+            ParticipantLimit = c.ParticipantLimit,
+            ActivityImageUrl = c.ActivityImageUrl,
+            ActivityTagCategory = c.ActivityTag.Category,
+            UserJoinActivityCount = c.UserJoinActivities.Count(uja => uja.Status == "Accept"),
+            UserJoinActivity = c.UserJoinActivities
+                .Where(uja => uja.Status == "Accept")
+                .Select(uja => new UserJoinActivity
+                {
+                    Status = uja.Status,
+                    User = new User { UserImageUrl = uja.User.UserImageUrl }
+                }).ToList()
+        })
+        .ToListAsync();
+
+    return Json(activities);
+}
+
+
 }
